bbafa3784d7e376adbea4a6c8d9e92eed38cdacf
Aleksey Makarov <aleksey.makarov@auriga.com>
2015-12-02 18:48:49 +0300

    chapter 9 part 1

--- geturlscancel.hs
+++ geturlscancel.hs
@@ -1,3 +1,8 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-3.15 --install-ghc runghc --package HTTP
+
+{-# OPTIONS_GHC -Wall #-}
+
 -- (c) Simon Marlow 2011, see the file LICENSE for copying terms.
 --
 -- Sample geturls.hs (CEFP summer school notes, 2011)
@@ -18,7 +23,6 @@ import Control.Concurrent
 import Control.Exception
 import Text.Printf
 import qualified Data.ByteString as B
-import Prelude hiding (catch)
 
 -----------------------------------------------------------------------------
 -- Our Async API:
@@ -42,33 +46,43 @@ waitCatch (Async _ var) = readMVar var
 
 -- <<cancel
 cancel :: Async a -> IO ()
-cancel (Async t var) = throwTo t ThreadKilled
+cancel (Async t _) = throwTo t ThreadKilled
 -- >>
 
 -----------------------------------------------------------------------------
 
-sites = ["http://www.google.com",
+sites :: [[Char]]
+sites = [
          "http://www.bing.com",
-         "http://www.yahoo.com",
-         "http://www.wikipedia.com/wiki/Spade",
-         "http://www.wikipedia.com/wiki/Shovel"]
+         "http://www.google.com",
+         "http://yandex.ru",
+         "http://www.yahoo.ru", -- unsuccessful one (https is not supported)
+         "http://chimera.labs.oreilly.com/books/1230000000929/ch08.html",
+         "http://chimera.labs.oreilly.com/books/1230000000929/ch09.html"
+        ]
 
-timeDownload :: String -> IO ()
-timeDownload url = do
-  (page, time) <- timeit $ getURL url
-  printf "downloaded: %s (%d bytes, %.2fs)\n" url (B.length page) time
+timeDownload :: String -> IO (B.ByteString, Double)
+timeDownload url = timeit $ getURL url
 
 -- <<main
+main :: IO ()
 main = do
   as <- mapM (async . timeDownload) sites                     -- <1>
 
-  forkIO $ do                                                 -- <2>
-     hSetBuffering stdin NoBuffering
+  void $ forkIO $ do                                                 -- <2>
+     void $ hSetBuffering stdin NoBuffering
      forever $ do
         c <- getChar
         when (c == 'q') $ mapM_ cancel as
 
   rs <- mapM waitCatch as                                     -- <3>
+
+  mapM_ f $ zip sites rs
+
   printf "%d/%d succeeded\n" (length (rights rs)) (length rs) -- <4>
+
+    where
+      f (n, (Left e))       = printf "FAIL: %s %s\n" n (show e)
+      f (n, (Right (s, t))) = printf "OK:   %s time: %f, length: %d\n" n t (B.length s)
 -- >>
 
